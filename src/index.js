f0b15926054a6aa6e1d9db8fa47699d8d4aae081
Torlapatichandu <tchandu9849@gmail.com>
2024-07-11 14:27:14 +0530

    updates

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Register from './components/register';
+import Home from './components/home';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <Router>
+      <Navbar/>
+      <h1>Web App</h1>
+      <Home/>
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<h1>Login</h1>}/>
+        <Route path="/logout" element={<h1>Logout</h1>}/>
+      </Routes>
+    </Router>
+  </div>
+);
+
